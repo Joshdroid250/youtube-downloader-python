97991ad74d55203b76e5d9dfac17a165075256e2
Joshdroid250 <gutierrez25g@gmail.com>
2024-09-18 14:37:01 -0600

    minior fix

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ yt = YouTube(input_url)
 print(f'El titulo de video a descargar es: {yt.title}')
 print(f'Escoge que video descargar: {yt.streams.filter(file_extension='mp4')}')
 
+
+
 try:
     stream = yt.streams.get_by_itag(input("Ingresa el itag del video a descargar: "))
     stream.download()   # Download the video
